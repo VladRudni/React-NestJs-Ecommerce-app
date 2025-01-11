0013e458e3c6a59ef57d053d585bdc97e44d6fb3
VladRudni <131704848+VladRudni@users.noreply.github.com>
2025-01-11 14:03:17 +0200

    add prisma client and configure servise prisma

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router";
+import { MainPage } from "./pages/MainPage";
+import { AboutPage } from "./pages/AboutPage";
+import { Button } from "./componets/ui/Button";
 
 function App() {
-  return <>123123 hrllooo</>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/">Main</Link>
+        <Link to="/about">about</Link>
+      </div>
+      <Button buttonId="123123">
+        Hello world
+        <i>hello</i>
+      </Button>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { ProductModule } from './product/product.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DB,
-      models: [],
-      autoLoadModels: true,
-    }),
+    ProductModule,
+    PrismaModule,
   ],
 })
 export class AppModule {}
